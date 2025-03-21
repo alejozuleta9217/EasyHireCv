0dd678753961af3b0884d0d957e7b0874a11d669
Alejandro Zuleta <alejozuleta9217@gmail.com>
2025-03-21 11:33:15 -0500

    Fix GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/EasyHireCv/',  // 🔹 Reemplaza con el nombre de tu repo en GitHub
   plugins: [react()],
 })
-
 
